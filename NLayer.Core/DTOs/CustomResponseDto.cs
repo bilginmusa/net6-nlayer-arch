6e2413bc54aef1c156d90b244f53e0bf20d69712
Musa Bilgin <musa_bilgin_55@hotmail.com>
2022-01-20 13:51:58 +0300

    CustomResponseDto added

---
+++ NLayer.Core/DTOs/CustomResponseDto.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NLayer.Core.DTOs
+{
+    public class CustomResponseDto<T>
+    {
+        public T? Data { get; set; }
+        public List<string>? Errors { get; set; }
+
+        [JsonIgnore]
+        public int StatusCode { get; set; }
+
+        public static CustomResponseDto<T> Success(int statusCode, T data) => new() { StatusCode = statusCode, Data = data };
+        public static CustomResponseDto<T> Success(int statusCode) => new() { StatusCode = statusCode };
+        public static CustomResponseDto<T> Fail(List<string> errors, int statusCode) => new() { StatusCode = statusCode, Errors = errors };
+        public static CustomResponseDto<T> Fail(string error, int statusCode) => new() { StatusCode = statusCode, Errors = new() { error } };
+    }
+}
