d2b87649e5bb300acbcfe342ad461840b6f3e170
Musa Bilgin <musa_bilgin_55@hotmail.com>
2022-02-09 15:30:28 +0300

    Product Controller modify.

--- NLayer.Repository/UnitOfWorks/UnitOfWork.cs
+++ NLayer.Repository/UnitOfWorks/UnitOfWork.cs
@@ -5,6 +5,11 @@ namespace NLayer.Repository.UnitOfWorks
     public class UnitOfWork : IUnitOfWorks
     {
         private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+                _context= context;
+        }
         public void Commit()
         {
             _context.SaveChanges();
